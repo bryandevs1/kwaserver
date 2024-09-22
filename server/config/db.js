0ab10d5b83b7863dd13b19445ee915febea78f3d
Bryan <bryanojji4@gmail.com>
2024-09-22 07:54:20 +0100

    mongoose

--- server/config/db.js
+++ server/config/db.js
@@ -1,15 +1,23 @@
-const mongoose = require('mongoose');
-const connectDB = async()=> {
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
   try {
-    mongoose.set('strictQuery', false);
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    mongoose.set("strictQuery", false);
+
+    // Directly specify the MongoDB URI here
+    const conn = await mongoose.connect(
+      "mongodb+srv://asinma209:QGSzk3RuJl8brSxa@cluster0.uezam.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+
     console.log(`Database Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.log(error);
+    console.log(`Error: ${error.message}`);
+    process.exit(1); // Exit process with failure
   }
-}
-
-
+};
 
 module.exports = connectDB;
-
