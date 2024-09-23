ee6ae4c7f189aaad5c76d7db5c7d249eef13f83a
Bryan <bryanojji4@gmail.com>
2024-09-23 17:59:11 +0100

    port

--- server/config/db.js
+++ server/config/db.js
@@ -1,8 +1,11 @@
 const mongoose = require("mongoose");
+
 const connectDB = async () => {
   try {
     mongoose.set("strictQuery", false);
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    const conn = await mongoose.connect(
+      "mongodb+srv://asinma209:QGSzk3RuJl8brSxa@cluster0.uezam.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    );
     console.log(`Database Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
