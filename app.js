710934835826a5ea72574359c2800d644b90ecfc
Bryan <bryanojji4@gmail.com>
2024-09-23 15:28:01 +0100

    port

--- app.js
+++ app.js
@@ -81,7 +81,7 @@ app.get("*", (req, res) => {
   res.status(404).render("404");
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
